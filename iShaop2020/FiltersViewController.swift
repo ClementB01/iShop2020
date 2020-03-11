35a340fb13a81fda472f77b1ab416cd05081371e
BAR CLEMENT p1928960 <clement.bar@etu.univ-lyon1.fr>
2020-03-11 17:47:05 +0100

    manage slider value

--- iShaop2020/FiltersViewController.swift
+++ iShaop2020/FiltersViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class FiltersViewController: UITableViewController {
-
+        
+    @IBOutlet weak var txtPrice: UILabel!
+    @IBOutlet weak var slidePrice: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,73 +21,20 @@ class FiltersViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let minValue: Float = 0.0
+        let maxValue: Float = 200.0
+        
+        slidePrice.minimumValue = minValue
+        slidePrice.maximumValue = maxValue
+        slidePrice.value = slidePrice.maximumValue/2
+        
+        txtPrice.text = String(slidePrice.value) + "€"
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    // MARK: - Actions
+    
+    @IBAction func onValueChange(_ sender: Any) {
+        txtPrice.text = String(slidePrice.value) + "€"
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
