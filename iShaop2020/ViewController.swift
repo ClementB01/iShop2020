0ad95bfcf7daa8a128f9e819712eafdbadab3ec9
ClementB01 <bar.clement01@gmail.com>
2020-05-13 21:21:36 +0200

    redo mainstoryboard

--- iShaop2020/ViewController.swift
+++ iShaop2020/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var dataManager: CoreDataManager {
         get {
